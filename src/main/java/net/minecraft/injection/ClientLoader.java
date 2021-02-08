738fd6f1418a74c775fa9046eab2fdfe1d644a24
Linus Torvalds <torvalds@linux-foundation.org>
2021-02-08 09:08:46 +0000

    整理代码

--- src/main/java/net/minecraft/injection/ClientLoader.java
+++ src/main/java/net/minecraft/injection/ClientLoader.java
@@ -1,7 +1,10 @@
 package net.minecraft.injection;
 
 import cn.snowflake.rose.Client;
+import cn.snowflake.rose.transform.ClassTransformer;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import org.objectweb.asm.Type;
+
 import java.util.Map;
 
 public class ClientLoader implements IFMLLoadingPlugin {
@@ -9,7 +12,7 @@ public class ClientLoader implements IFMLLoadingPlugin {
 
 	@Override
 	public String[] getASMTransformerClass() {
-		return new String[] {"cn.snowflake.rose.asm.ClassTransformer"};
+		return new String[] {Type.getInternalName(ClassTransformer.class).replace("/",".")};
 	}
 
 	@Override
